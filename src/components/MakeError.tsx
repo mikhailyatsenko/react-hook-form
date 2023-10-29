5056617ff438e70928f47726459a725f2b2bff98
mikhailyatsenko <misha.ggg@gmail.com>
2023-10-29 20:57:21 +0100

    fix error boundary code

--- src/components/MakeError.tsx
+++ src/components/MakeError.tsx
@@ -5,17 +5,17 @@ interface State {
 }
 
 class MakeError extends React.Component<Record<string, never>, State> {
+  throwError = () => {
+    this.setState({
+      error: true,
+    });
+  };
   render() {
-    const throwError = () => {
-      this.setState({
-        error: true,
-      });
-    };
     if (this.state) {
       throw Error();
     }
     return (
-      <button className="error-button" onClick={throwError}>
+      <button className="error-button" onClick={this.throwError}>
         Throw Error
       </button>
     );
